7257c11ac9e61c020348c7bbaa42810d1d36bc56
Sasha Price <sashaprice@unomaha.edu>
2020-11-08 21:06:19 -0600

    Adding data model, required jars

---
+++ term-project/src/model/Question.java
@@ -0,0 +1,47 @@
+package model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "question")
+public class Question {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+    
+    @Column(name = "prompt", nullable = false)
+    private String prompt;
+    
+    @OneToMany(mappedBy = "id")
+    private Set<Choice> choices;
+    
+    public Question(int id, String prompt) {
+        setId(id);
+        setPrompt(prompt);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPrompt() {
+        return prompt;
+    }
+
+    public void setPrompt(String prompt) {
+        this.prompt = prompt;
+    }
+}
---
+++ term-project/src/model/Survey.java
@@ -0,0 +1,59 @@
+package model;
+
+import java.util.Set;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "survey")
+public class Survey {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    private int id;
+    
+    @Column(name = "name", nullable = false)
+    private String name;
+    
+    @Column(name = "description", nullable = false)
+    private String description;
+    
+    @OneToMany(mappedBy = "id")
+    private Set<Question> questions;
+    
+    public Survey(int id, String name, String description) {
+        setId(id);
+        setName(name);
+        setDescription(description);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
